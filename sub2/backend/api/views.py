1adabc918b250f1e109350e945db61f5334374ba
김태민 <taeminkim23@gmail.com>
2020-04-17 17:13:15 +0900

    add nearbyStoreREST
    Former-commit-id: 8985509ce6d00abe0adc62176cedcb77eee9d57b

--- sub2/backend/api/views.py
+++ sub2/backend/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
 from rest_framework import status
+from haversine import haversine
 
 
 class SmallPagination(PageNumberPagination):
@@ -173,3 +174,41 @@ class reviewCRUD(APIView):
             return Response({'status': status.HTTP_200_OK})
         except :
             return Response({'status': status.HTTP_400_BAD_REQUEST})
+
+
+class searchNearbyStore(APIView):
+    def post(self, request):
+
+        ## session의 값과 userId값이 같으지 확인해야함^^^^^^^^
+        if ('latitude' in request.POST.keys()) and ('longitude' in request.POST.keys()) and ('distance' in request.POST.keys()):
+            curla = request.POST['latitude']
+            curlo = request.POST['longitude']
+            distance = request.POST['distance']
+
+            mile = float(distance) * 0.621371
+            
+            # queryset = Store.objects.all().filter(self.calculateMeter(curla,curlo,latitude,longitude.distance))
+            # queryset = Store.objects.all().filter(id=149)
+            queryset = Store.objects.raw("SELECT *, ( 3959 * acos( cos( radians("
+                + str(curla) +") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians("
+                + str(curlo) +") ) + sin( radians("
+                + str(curla) +") ) * sin( radians( latitude ) ) ) ) as distance from api_store Having distance <"
+                + str(mile) +";")
+            serializer = serializer = StoreSerializer(queryset, many = True)
+
+            return Response(serializer.data)
+        else :
+            return Response({'status': status.HTTP_400_BAD_REQUEST})
+            
+    def calculateMeter(self,curx,cury,x,y,distance):
+
+        curPosition = (curx,cury)
+        position = (x,y)
+
+        meter = haversine(curPosition,position)
+
+        if(meter <= distance):
+            return True
+        else:
+            return False
+
