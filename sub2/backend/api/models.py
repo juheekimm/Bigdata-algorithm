d9e8d9747026163ffd758d35782f57b810936a02
jason07999 <jason07999@naver.com>
2020-04-18 16:42:25 +0900

    dt
    Former-commit-id: ae056c6892b8ff386df218e19b9dea93e3d70a24

--- sub2/backend/api/models.py
+++ sub2/backend/api/models.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 from accounts.models import Profile
+import datetime
 
 class Store(models.Model):
     id = models.AutoField(primary_key=True)
