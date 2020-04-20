4cef3b91ed7451ce7168f344d1246e45cbb97a85
김주희 <yylcd9999@naver.com>
2020-04-20 14:35:38 +0900

    edit db profile table and migrate
    Former-commit-id: f0ae917fc4e18817f242d150f46fffecc1c7c590

--- sub2/backend/accounts/forms.py
+++ sub2/backend/accounts/forms.py
@@ -13,4 +13,4 @@ class UserForm(forms.ModelForm):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('gender', 'age')
+        fields = ('nickname', 'gender', 'age')
