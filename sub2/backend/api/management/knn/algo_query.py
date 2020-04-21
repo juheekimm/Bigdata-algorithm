277cbd8254949bf156f8dba30c5bc3a3d37efe51
김주희 <yylcd9999@naver.com>
2020-04-21 18:27:58 +0900

    add knn test
    Former-commit-id: 5c1658d25baa24220cb582fca9d5e54710ff0f2c

--- sub2/backend/api/management/knn/algo_query.py
+++ sub2/backend/api/management/knn/algo_query.py
@@ -1,13 +1,13 @@
 # query1
 
-# select count(store_id), avg(total_score), store_id
+# select	count(store_id), avg(total_score), store_id
 # from		api_review r
 # join		accounts_profile p
 # on			r.user_id = p.id
-#             and p.age >= 1985
-#             and p.age < 1995
-#             and p.gender='여'
+# 			and p.age >= year(now()) - (age - 1)
+# 			and p.age < year(now()) - (age + 8)
+#             and p.gender=gender
 # group by	store_id
 # having		count(store_id) >= 2
-#             and avg(total_score) >= 4
+# 			and avg(total_score) >= 4
 # limit		5;
