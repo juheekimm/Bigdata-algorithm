060069efc94be6e90cebe2888a8f8d3ac7b0f93b
김주희 <yylcd9999@naver.com>
2020-04-22 16:59:26 +0900

    add mf recommend algorithm
    Former-commit-id: 333c0a1ebc574971983e1a2c6395dce78992e59a

---
+++ sub2/backend/api/management/knn/mf_algo.py
@@ -0,0 +1,62 @@
+import pandas as pd
+# import sklearn as sk
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sklearn.metrics.pairwise import cosine_similarity
+
+def query_MySqlDB(query):
+    # sqlalchemy engine
+    engine = create_engine(URL(
+        drivername="mysql",
+        username="root",
+        password="ssafy",
+        host="52.79.223.182",
+        port="3306",
+        database="django_test",
+        query = {'charset': 'utf8mb4'}
+    ))
+
+    conn = engine.connect()
+
+    generator_df = pd.read_sql(sql=query,  # mysql query
+                            con=conn)
+                            # chunksize = 3
+                            # size you want to fetch each time
+
+    # for dataframe in generator_df:
+        # for row in dataframe:
+        #     pass
+        # whatever you want to do
+
+    # print(generator_df)
+    return generator_df
+
+user_data = query_MySqlDB("select id as user_id, gender, age from accounts_profile")
+review_data = query_MySqlDB("select user_id, store_id, total_score from api_review")
+store_data = query_MySqlDB("select id as store_id, store_name, address from api_store where address like '서울특별시 강남구%%'")
+
+review_store_data = pd.merge(review_data, store_data, on="store_id")        #store 이름, 아이디, 주소 조인
+user_review_rating = pd.merge(user_data, review_store_data, on="user_id")   #user_id, 성별, 나이, stroe 이름, 아이디, 주소 조인
+
+# print("-----------")
+# print(user_review_rating.head(2))
+
+review_user_rating = user_review_rating.pivot_table("total_score", index="store_name", columns="user_id")
+user_review_rating = user_review_rating.pivot_table("total_score", index="user_id", columns="store_name")
+
+# print(user_review_rating.head(5))
+# print(review_user_rating.head(5))
+
+review_user_rating.fillna(0, inplace = True)
+# print(review_user_rating.head(5))
+
+item_based_collabor = cosine_similarity(review_user_rating)
+# print(item_based_collabor)
+
+item_based_collabor = pd.DataFrame(data = item_based_collabor, index = review_user_rating.index, columns=review_user_rating.index)
+print(item_based_collabor.head())
+
+def get_item_based_collabor(store_name):
+    return item_based_collabor[store_name].sort_values(ascending=False)[:10]
+
+print(get_item_based_collabor('아트몬스터'))
